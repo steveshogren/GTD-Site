cc696dd6da2145249432a44eaf098014bda3bd48
Steve <madklowns@gmail.com>
2012-07-21 17:13:01 -0400

    adding max amount and removing excess code

--- src/webtest/views/common.clj
+++ src/webtest/views/common.clj
@@ -11,6 +11,7 @@
   [{:keys [loan_id amount max_amount description interest]}]
   [:input#loanName] (set-attr :value description)
   [:input#loanInterest] (set-attr :value interest)
+  [:.table_max_amount_text] (content (str max_amount))
   [:input#loanAmount] (set-attr :value amount))
 
 (deftemplate index2 "html/template2.html"
@@ -20,27 +21,4 @@
 (defpartial layout [loans]
   (index2 loans))
 
-#_(defpartial site-layout [& content]
-  (html5
-    [:head [:title "my site"]]
-    [:body [:div#wrapper content]]))
-
-(defpartial todo-item
-    [{:keys [loan_id amount max_amount description interest]}]
-  [:tr
-    [:td.tabletext (text-field "input_name" description)]
-    [:td.table_interest (text-field "input_interest" interest)]
-    [:td.tabletext (text-field "input_amount" amount)]
-    [:td.table_max_amount_text max_amount]
-     ])
-
-
-#_(defpartial todos-list [items]
-  [:table#table
-   [:tr
-    [:th "Loan Name"]
-    [:th "Interest"]
-    [:th "Amount"]
-    [:th "Max"]]
-   (map todo-item items)])
 
--- src/webtest/views/welcome.clj
+++ src/webtest/views/welcome.clj
@@ -17,12 +17,6 @@
   (common/layout
     [:p "Welcome to webtest"]))
 
-#_(defpage "/my-page" []
-  (common/site-layout
-    [:h1 "welcome to my site!"]
-    [:p "hope you like it."]))
-
-
 (defpage "/todos" {}
   (common/layout (select loan (where {:user_id 17 :savings 0}) (order :interest :DESC) (order :amount :DESC))))
 
