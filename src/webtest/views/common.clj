5788f141016dbb7d35103bb84d2cbb49ac3c861f
Steve <madklowns@gmail.com>
2012-07-31 21:23:49 -0400

    finishing the thermometer sidebar and reversing the temperature to match the typical 'hit the top when you are finished' style

--- src/webtest/views/common.clj
+++ src/webtest/views/common.clj
@@ -6,7 +6,6 @@
         [hiccup.core]
         [net.cgrand.enlive-html]))
 
-#_([clojure.Contrib.math :only [floor]])
 (def loan-row-sel [[:form#update (nth-of-type 1)]])
 
 (defsnippet row-model "html/template2.html" loan-row-sel
@@ -25,13 +24,24 @@
 (defn loanPayoffPercentage [loans]
   (round (* 100 (/ (totalRemaining loans) (totalMaxRemaining loans)))))
 
+(defn thermometer-pixel [loans]
+  (/ (* 300 (- 100 (loanPayoffPercentage loans))) 100))
+
+(import '(java.text NumberFormat)
+        '(java.util Locale))
+(defn commify
+  ([n] (commify n (Locale/US)))
+  ([n locale]
+     (.format (NumberFormat/getInstance locale) (bigdec n))))
+
 (deftemplate index2 "html/template2.html"
   [loans]
   [:form#update] (content (map row-model loans))
-  [:span#totalLoanAmount] (content (str (totalRemaining loans)))
-  [:span#totalLoanAmount2] (content (str (totalRemaining loans)))
-  [:span#percentageLeft1] (content (str (loanPayoffPercentage loans)))
-  [:h2#cdg_h2] (content (str (loanPayoffPercentage loans))))
+  [:span#totalMaxAmount] (content (commify (totalMaxRemaining loans)))
+  [:span#totalLoanAmount] (content (commify (totalRemaining loans)))
+  [:#cdg_m] (set-attr :style (str "height: " (thermometer-pixel loans) "px;"))
+  [:#cdg_p] (set-attr :style (str "margin-bottom: " (thermometer-pixel loans) "px;"))
+  [:h2#cdg_h2] (content (str "Percent remaining: " (loanPayoffPercentage loans) "%")))
 
 
 (defpartial layout [loans]
